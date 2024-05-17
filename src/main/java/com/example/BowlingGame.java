1f6230d5b0b5e5adfaa963fbc7bb92d63feb33d9
junfeng.ren <junfeng.ren@thoughtworks.com>
2024-05-17 15:45:03 +0800

    fix test - 3

--- src/main/java/com/example/BowlingGame.java
+++ src/main/java/com/example/BowlingGame.java
@@ -2,13 +2,29 @@ package com.example;
 
 public class BowlingGame {
 
-    private int score;
+    private final int [] rolls = new int[21];
+
+    private int currentRoll = 0;
 
     public void roll(int pins) {
-        score += pins;
+        rolls[currentRoll++] = pins;
     }
 
     public int getScore() {
+        int score = 0;
+        int frameIndex = 0;
+        for(int frame = 0; frame < 10; frame++){
+            if(isSpare(frameIndex)){
+                score += 10 + rolls[frameIndex+2];
+            } else {
+                score += rolls[frameIndex] + rolls[frameIndex+1];
+            }
+            frameIndex += 2;
+        }
         return score;
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex+1] == 10;
     }
 }
