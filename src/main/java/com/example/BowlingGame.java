c11c120b7040c8adcddf97bac97606c09d07b810
junfeng.ren <junfeng.ren@thoughtworks.com>
2024-05-17 15:48:04 +0800

    fix test - 4

--- src/main/java/com/example/BowlingGame.java
+++ src/main/java/com/example/BowlingGame.java
@@ -14,17 +14,25 @@ public class BowlingGame {
         int score = 0;
         int frameIndex = 0;
         for(int frame = 0; frame < 10; frame++){
-            if(isSpare(frameIndex)){
+            if(isStrike(frameIndex)){
+                score += 10 + rolls[frameIndex+1]+rolls[frameIndex+2];
+                frameIndex += 1;
+            }else if(isSpare(frameIndex)){
                 score += 10 + rolls[frameIndex+2];
+                frameIndex += 2;
             } else {
                 score += rolls[frameIndex] + rolls[frameIndex+1];
+                frameIndex += 2;
             }
-            frameIndex += 2;
         }
         return score;
     }
 
     private boolean isSpare(int frameIndex) {
         return rolls[frameIndex] + rolls[frameIndex+1] == 10;
+    }
+
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex] == 10;
     }
 }
